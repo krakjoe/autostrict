224d6ccb8778ec6306f73ece88c0635ed2589788
Tom Van Looy <tom@ctors.net>
2016-02-04 00:10:43 +0100

    respect tabs

--- auto.c
+++ auto.c
@@ -96,7 +96,7 @@ ZEND_EXT_API zend_extension zend_extension_entry = {
 	"Joe Watkins <krakjoe@php.net>",
 	"https://github.com/krakjoe/autostrict",
 	"Copyright (c) 2016",
-	zend_auto_start,	/* startup_func_t */
+	zend_auto_start,		/* startup_func_t */
 	NULL,           	/* shutdown_func_t */
 	NULL,               /* activate_func_t */
 	NULL,               /* deactivate_func_t */
